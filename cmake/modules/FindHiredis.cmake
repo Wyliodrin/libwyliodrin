f9b11037e6391b877c7eabdc43605f4d5e2532c1
Brendan Le Foll <brendan.le.foll@intel.com>
2014-06-19 12:49:05 +0100

    cmake: fix hiredis and jannsson not being found and linked
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- cmake/modules/FindHiredis.cmake
+++ cmake/modules/FindHiredis.cmake
@@ -1,15 +1,31 @@
-find_path(HIREDIS_INCLUDE_DIR
-NAMES
-hiredis/hiredis.h
-PATHS
-/usr/include
-/usr/local/include
-)
+# FindHiredis.cmake - Try to find the Hiredis library
+# Once done this will define
+#
+#  HIREDIS_FOUND - System has Hiredis
+#  HIREDIS_INCLUDE_DIR - The Hiredis include directory
+#  HIREDIS_LIBRARIES - The libraries needed to use Hiredis
+#  HIREDIS_DEFINITIONS - Compiler switches required for using Hiredis
 
-find_library(HIREDIS_LIBRARY
-NAMES
-hiredis
-PATHS
-/usr/lib
-/usr/local/lib
-)
+#SET(HIREDIS_DEFINITIONS ${PC_HIREDIS_CFLAGS_OTHER})
+
+FIND_PATH(HIREDIS_INCLUDE_DIR NAMES hiredis/hiredis.h
+   HINTS
+   /usr
+   /usr/local
+   /opt
+   PATH_SUFFIXES hiredis
+   )
+
+FIND_LIBRARY(HIREDIS_LIBRARIES NAMES hiredis
+   HINTS
+   /usr
+   /usr/local
+   /opt
+   )
+
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Hiredis DEFAULT_MSG HIREDIS_LIBRARIES HIREDIS_INCLUDE_DIR)
+
+MARK_AS_ADVANCED(HIREDIS_INCLUDE_DIR HIREDIS_LIBRARIES)
+
