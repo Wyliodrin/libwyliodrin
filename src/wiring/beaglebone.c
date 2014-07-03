493f3e775a669af11fa3ea566bccf9c3069c73bd
Matei94 <matei.rm94@gmail.com>
2014-07-03 20:47:25 +0300

    beaglebone: first skeleton

--- src/wiring/beaglebone.c
+++ src/wiring/beaglebone.c
@@ -1,5 +1,33 @@
 #ifdef BEAGLEBONE
 
-  // TODO Relax
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+void pinMode (int pin, int mode) {
+  // TODO
+}
+
+void digitalWrite (int pin, int value) {
+  // TODO
+}
+
+int digitalRead (int pin) {
+  // TODO
+
+  return 0;
+}
+
+void analogWrite (int pin, int value) {
+  // TODO
+}
+
+int analogRead (int pin) {
+  // TODO
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BEAGLEBONE
