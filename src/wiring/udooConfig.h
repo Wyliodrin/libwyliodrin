ff1c9b9573db578a47254fdf7f0631fe76bedd5a
Andrei Dinu <mandrei.dinu@gmail.com>
2014-07-17 11:31:01 +0300

    introduce debug macro

--- src/wiring/udooConfig.h
+++ src/wiring/udooConfig.h
@@ -7,6 +7,7 @@
  * CONTENT:
  * 1. Constants & Flags
  * 2. GPIO
+ * 3. Debug
  *************************************************************************************************/
 
 
@@ -27,15 +28,15 @@ extern "C" {
 #define GPIO_FILE_EXPORT GPIO_FILE_PREFIX "export"
 
 // Direction
-#define GPIOF_DIR_IN  "in"    // to configure direction as input
-#define GPIOF_DIR_OUT "out"   // to configure direction as output 
+#define GPIOF_DIR_IN  "in"     // to configure direction as input
+#define GPIOF_DIR_OUT "out"    // to configure direction as output 
 
 // Values
 #define HIGH 1
 #define LOW  0
 
-#define GPIOF_INIT_LOW  LOW   // as output, set initial level to LOW
-#define GPIOF_INIT_HIGH HIGH  // as input, set initial level to HIGH
+#define GPIOF_INIT_LOW  LOW    // as output, set initial level to LOW
+#define GPIOF_INIT_HIGH HIGH   // as input, set initial level to HIGH
 
 // Edge
 enum whatEdge {
@@ -70,14 +71,16 @@ byte getGpioByArdFunc   (const char* ardFunction);
 
 byte getGpioByKey       (const char* key);
 
-/* test if some number could reference a correct GPIO */
+// test if some number could reference a correct GPIO.
 byte gpioIsValid        (byte gpio);
+
 byte gpioIsExported     (byte gpio);
 
-/* set as input or output, returning 0 or negative errno */
+// set as input or output, returning 1 or negative errno
 int gpioSetDirInput     (byte gpio);
 int gpioSetDirOutput    (byte gpio, const char* value);
 
+// get the pin direction
 byte gpioGetDir         (byte gpio);
 
 int gpioGetValue        (byte gpio);
@@ -92,12 +95,32 @@ byte gpioGetEdge        (byte gpio);
 void gpioSetActiveLow   (byte gpio, byte value);
 byte gpioGetActiveLow   (byte gpio);
 
-/* map GPIO numbers to IRQ numbers */
+// map GPIO numbers to IRQ numbers
 int gpioToIrq           (byte gpio);
 
-/* map IRQ numbers to IRQ numbers */
+// map IRQ numbers to IRQ numbers
 int irqToGpio           (byte gpio);
-		
+
+
+
+/**************************************************************************************************
+ * 3. Debug
+ *************************************************************************************************/
+
+/**
+ * Took this from Matei. It's a nice macro that does all the work good.
+ */
+
+#define DEBUG 1
+#define debug(...)                                                                 \
+  do {                                                                             \
+    if (DEBUG) {                                                                   \
+      fprintf(stderr, "Debug alert in file %s at line %d:\n", __FILE__, __LINE__); \
+      fprintf(stderr, __VA_ARGS__);                                                \
+      fprintf(stderr, "\n");                                                       \
+    }                                                                              \
+  } while (0)                                                                      \
+
 
 #ifdef __cplusplus
 }
