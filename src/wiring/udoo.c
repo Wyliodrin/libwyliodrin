492139aa94dcf55346c7f01d1d16d65cad19f145
Andrei Dinu <mandrei.dinu@gmail.com>
2014-07-18 09:54:04 +0300

    introduce udoo configuration file

---
+++ src/wiring/udoo.c
@@ -0,0 +1,24 @@
+/**************************************************************************************************
+ * This file contains the functions available on UDOO pin headers.
+ * See documentation here: 
+ * 		http://arduino.cc/en/Reference/HomePage
+ * 		file:///home/andrei/Desktop/UDOO_Starting_Manual_beta0.4_11_28_2013.pdf
+ *
+ * Follow "Wyliodrin Coding Style Convention" at:
+ *  https://docs.google.com/document/d/14zRCX1KIwvQ1qEzWBVH-We0CkQmd5-kZb81bzvbIQKY/edit
+ * 
+ * CONTENT:
+ * 1.  General configuration
+ * 2.  Wiring
+ * 3.  UARTs: uart1, uart3, uart4, uart5
+ * 4.  SD1
+ * 5.  SPIs: spi1, spi2, spi5
+ * 6.  I2C1
+ * 7.  Spdif
+ * 8.  Timer capture
+ * 9.  Timer compare
+ * 10. WATCHDOG FUNCTIONALITIES: watchdog reset, watchdog out
+ * 11. Clock out
+ * 12. PWMs: pwm1, pwm2, pwm3, pwm4
+ * 13. Digital audio 
+ *************************************************************************************************/
