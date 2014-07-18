ebaa7bb905e589e04aff9ea4ba0ce264714f6f2c
Andrei Dinu <mandrei.dinu@gmail.com>
2014-07-18 10:17:11 +0300

    introduce udoo configuration file - code refactoring

--- src/wiring/udoo.c
+++ src/wiring/udoo.c
@@ -5,20 +5,44 @@
  * 		file:///home/andrei/Desktop/UDOO_Starting_Manual_beta0.4_11_28_2013.pdf
  *
  * Follow "Wyliodrin Coding Style Convention" at:
- *  https://docs.google.com/document/d/14zRCX1KIwvQ1qEzWBVH-We0CkQmd5-kZb81bzvbIQKY/edit
+ *  	https://docs.google.com/document/d/14zRCX1KIwvQ1qEzWBVH-We0CkQmd5-kZb81bzvbIQKY/edit
  * 
  * CONTENT:
  * 1.  General configuration
- * 2.  Wiring
- * 3.  UARTs: uart1, uart3, uart4, uart5
- * 4.  SD1
- * 5.  SPIs: spi1, spi2, spi5
- * 6.  I2C1
- * 7.  Spdif
- * 8.  Timer capture
- * 9.  Timer compare
- * 10. WATCHDOG FUNCTIONALITIES: watchdog reset, watchdog out
- * 11. Clock out
- * 12. PWMs: pwm1, pwm2, pwm3, pwm4
- * 13. Digital audio 
+ * 2.  Digital I/O
+ * 3.  Analog I/O
+ * 3.  Advanced I/O
+ * 4.  Time
+ * 5.  Communication (Serial, Stream)
+ * 6.  UARTs: uart1, uart3, uart4, uart5
+ * 7.  SD1
+ * 8.  SPIs: spi1, spi2, spi5
+ * 9.  I2C1
+ * 10. Spdif
+ * 11. Timer capture
+ * 12. Timer compare
+ * 13. WATCHDOG FUNCTIONALITIES: watchdog reset, watchdog out
+ * 14. Clock out
+ * 15. PWMs: pwm1, pwm2, pwm3, pwm4
+ * 16. Digital audio 
  *************************************************************************************************/
+
+#ifdef UDOO
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "wiring.h"
+#include "udooConfig.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UDOO */
