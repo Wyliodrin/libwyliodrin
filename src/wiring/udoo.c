a42c110718492f681701d2d478e66d5413df7468
Andrei Dinu <mandrei.dinu@gmail.com>
2014-07-19 11:33:58 +0300

    Configure Digital I/O functions

--- src/wiring/udoo.c
+++ src/wiring/udoo.c
@@ -1,11 +1,11 @@
 /**************************************************************************************************
  * This file contains the functions available on UDOO pin headers.
  * See documentation here: 
- * 		http://arduino.cc/en/Reference/HomePage
- * 		file:///home/andrei/Desktop/UDOO_Starting_Manual_beta0.4_11_28_2013.pdf
+ *      http://arduino.cc/en/Reference/HomePage
+ *      file:///home/andrei/Desktop/UDOO_Starting_Manual_beta0.4_11_28_2013.pdf
  *
  * Follow "Wyliodrin Coding Style Convention" at:
- *  	https://docs.google.com/document/d/14zRCX1KIwvQ1qEzWBVH-We0CkQmd5-kZb81bzvbIQKY/edit
+ *      https://docs.google.com/document/d/14zRCX1KIwvQ1qEzWBVH-We0CkQmd5-kZb81bzvbIQKY/edit
  * 
  * CONTENT:
  * 1.  General configuration
@@ -37,9 +37,57 @@ extern "C" {
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "wiring.h"
 #include "udooConfig.h"
 
+
+/**************************************************************************************************
+ * 2. Digital I/O
+ *************************************************************************************************/
+
+/**
+ * Configures the specified pin to behave either as an input or an output
+ * pin: the number of the pin whose mode you wish to set (gpio pin)
+ * mode: INPUT or OUTPUT
+ */
+
+void pinMode (int pin, int mode) 
+{
+    if (mode != INPUT && mode != OUTPUT) {
+        debug("Mode can be either INPUT or OUTPUT");
+        return;
+    }
+    gpioExport(pin);
+    if (mode == INPUT) {
+        gpioSetDirInput(pin);
+    } else {
+        gpioSetDirOutput(pin);
+    }
+}
+
+/**
+ * Write a HIGH or a LOW value to a pin
+ * pin: the pin number (gpio pin)
+ * value: HIGH or LOW
+ */
+
+void digitalWrite (int pin, int value)
+{
+    gpioSetValue(pin, value);
+}
+
+/** 
+ * Reads the value from a specified digital pin, either HIGH or LOW
+ * pin: the number of the digital pin you want to read
+ */
+
+int digitalRead (int pin)
+{
+    // See defined values in udooConfig.h for invalid/unexported pin that can be returned 
+    return gpioGetValue(pin);
+}
 
 #ifdef __cplusplus
 }
