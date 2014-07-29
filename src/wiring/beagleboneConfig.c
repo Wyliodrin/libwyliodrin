68b056fd853113edf5a0dbd67554a10656c96a3a
Matei94 <matei.rm94@gmail.com>
2014-07-29 22:15:12 +0300

    beaglebone: code refactoring part 3.1

--- src/wiring/beagleboneConfig.c
+++ src/wiring/beagleboneConfig.c
@@ -753,7 +753,7 @@ result_t ledSetValue(const byte led, const byte value) {
 /**
  * Returns value of gpio-led LOW or HIGH or 0xFF in case of failure
  */
-byte ledGetValue(const byte gpio) {
+byte ledGetValue(const byte led) {
   if(!(0 <= led && led <= 3)) {
     debug("Invalid led %d. Value of led should be in [0, 3] interval", led);
     return ERROR;
@@ -794,16 +794,16 @@ result_t ledReset(const byte led) {
 
   switch(led) {
     case 0:
-      ledSetTrigger(gpio, HEARTBEAT);
+      ledSetTrigger(led, HEARTBEAT);
       break;
     case 1:
-      ledSetTrigger(gpio, MMC0);
+      ledSetTrigger(led, MMC0);
       break;
     case 2:
-      ledSetTrigger(gpio, CPU0);
+      ledSetTrigger(led, CPU0);
       break;
     case 3:
-      ledSetTrigger(gpio, MMC1);
+      ledSetTrigger(led, MMC1);
       break;
     default:
       debug("This should not have happened");
