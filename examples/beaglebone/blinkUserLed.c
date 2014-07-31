45782d2f8c450092be754045a4b98273666811b6
Matei94 <matei.rm94@gmail.com>
2014-07-31 17:30:49 +0300

    beaglebone: example blinkUserLed.c

---
+++ examples/beaglebone/blinkUserLed.c
@@ -0,0 +1,27 @@
+/**************************************************************************************************
+ * Author: Razvan Madalin MATEI <matei.rm94@gmail.com>
+ * Date last modified: July 2014
+ *
+ * Turns ON and OFF on-board led USR0 for 10 times at 0.5 secs interval
+ *************************************************************************************************/
+
+#define NO_BLINKS 10
+
+int main() {
+  int i;
+  int USR0;
+
+  USR0 = getPinByKey("USR0");
+
+  pinMode(USR0, OUTPUT)
+
+  for(i = 0; i < NO_BLINKS; i++) {
+    digitalWrite(USR0, HIGH);
+    delay(500);
+    digitalWrite(USR0, LOW);
+    delay(500);
+  }
+
+  pinReset("USR0");
+  return 0;
+}
