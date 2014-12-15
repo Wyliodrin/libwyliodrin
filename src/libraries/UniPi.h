09c3386b393664c22debe9736c7df1ea5e2dea8d
Alexandru Radovici <msg4alex@gmail.com>
2014-12-15 16:27:32 +0200

    Update UniPi.h

--- src/libraries/UniPi.h
+++ src/libraries/UniPi.h
@@ -1,10 +1,18 @@
 #ifndef UNIPI_H
 #define UNIPI_H
 
+#ifdef __cplusplus /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
+
 void unipiSetup ();
 void unipiRelay (int relay, int value);
 void unipiAnalogWrite (int value);
 int unipiAnalogRead (int pin);
 int unipiDigitalRead (int pin);
+
+#ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
+}
+#endif
 
 #endif
