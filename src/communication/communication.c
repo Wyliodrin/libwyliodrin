8c42355f788faae144d8e3ba5a3acedf3d5b79b9
Razvan Madalin MATEI <matei.rm94@gmail.com>
2015-02-14 17:53:41 +0200

    doc: convert indentation to spaces

--- src/communication/communication.c
+++ src/communication/communication.c
@@ -35,10 +35,10 @@ char *redis_channel = "communication:";
  */
 void redisDisconnect (const redisAsyncContext *c, int status)
 {
-	if(status != REDIS_OK)
-	{
-		asyncConnect(PORT);
-	}
+  if(status != REDIS_OK)
+  {
+    asyncConnect(PORT);
+  }
 }
 
 /**
@@ -46,18 +46,18 @@ void redisDisconnect (const redisAsyncContext *c, int status)
  */
 int asyncConnect(int redis_port)
 {
-	c = redisAsyncConnect(IP, redis_port);
-	signal(SIGPIPE, SIG_IGN);
-	if (c != NULL && c->err)
-	{
-		return REDIS_ECONNECT;
-	} 
-	if(c == NULL)
-	{
-		return REDIS_ECONNECT;
-	}
-	redisAsyncSetDisconnectCallback(c, redisDisconnect);
-	return 0;
+  c = redisAsyncConnect(IP, redis_port);
+  signal(SIGPIPE, SIG_IGN);
+  if (c != NULL && c->err)
+  {
+    return REDIS_ECONNECT;
+  } 
+  if(c == NULL)
+  {
+    return REDIS_ECONNECT;
+  }
+  redisAsyncSetDisconnectCallback(c, redisDisconnect);
+  return 0;
 }
 
 /**
@@ -73,16 +73,16 @@ void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
 
   if (reply == NULL)
   {
-  	return;
+    return;
   }
   if (r->type == REDIS_REPLY_ARRAY) {
-   	for (j = 0; j < (r->elements-2); j = j+3) {
+    for (j = 0; j < (r->elements-2); j = j+3) {
       data = r->element[j]->str;
-    	if(strcmp(data, "message") == 0)
-    	{
-    		data = r->element[j+2]->str;
-    		f(0, data);
-    	}         
+      if(strcmp(data, "message") == 0)
+      {
+        data = r->element[j+2]->str;
+        f(0, data);
+      }         
     }
   }
 }
@@ -90,7 +90,7 @@ void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
 
 
 /**************************************************************************************************
- * 2. Communication
+ * 2. Communication 
  *************************************************************************************************/
 
 /**
@@ -98,25 +98,25 @@ void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
  */
 int initCommunication(int redis_port)
 {
-	int rc;
+  int rc;
 
-	rc = asyncConnect(redis_port);
-	if (rc != 0) 
-	{
-		return rc;
-	}
+  rc = asyncConnect(redis_port);
+  if (rc != 0) 
+  {
+    return rc;
+  }
 
-	context = redisConnect(IP, redis_port);
-	if (context != NULL && context->err)
-	{
-		return REDIS_ECONNECT;
-	} 
-	if(context == NULL)
-	{
-		return REDIS_ECONNECT;
-	}
+  context = redisConnect(IP, redis_port);
+  if (context != NULL && context->err)
+  {
+    return REDIS_ECONNECT;
+  } 
+  if(context == NULL)
+  {
+    return REDIS_ECONNECT;
+  }
 
-	return 0;
+  return 0;
 }
 
 /**
@@ -124,11 +124,11 @@ int initCommunication(int redis_port)
  */
 void openConnection (int communication_port, void (*f)(int err, char *data))
 {
-	struct event_base *base;
+  struct event_base *base;
 
-	base = base = event_base_new();
-	initCommunication(6379);
-	redisLibeventAttach(c, base);
+  base = base = event_base_new();
+  initCommunication(6379);
+  redisLibeventAttach(c, base);
   redisAsyncCommand(c, onMessage, f, "SUBSCRIBE %s%d", redis_channel,communication_port);
   event_base_dispatch(base);
 }
@@ -138,29 +138,29 @@ void openConnection (int communication_port, void (*f)(int err, char *data))
  */
 int sendMessage (char *id, int communication_port, char *data)
 {
-	char *j;
-	json_t *root;
-	redisReply *reply;
+  char *j;
+  json_t *root;
+  redisReply *reply;
 
-	root = json_object();
-	json_object_set_new(root, "id", json_string(id));
-	json_object_set_new(root, "data", json_string(data));
+  root = json_object();
+  json_object_set_new(root, "id", json_string(id));
+  json_object_set_new(root, "data", json_string(data));
 
-	j = json_dumps(root, 0);
-	if(j == NULL)
-	{
-		return -1;
-	}
+  j = json_dumps(root, 0);
+  if(j == NULL)
+  {
+    return -1;
+  }
 
-	reply = redisCommand(context, "PUBLISH %s%d %s", redis_channel, communication_port, j);
-	free(j);
-	if(reply)
-	{
-		freeReplyObject(reply);
-		return 0;
-	} else {
-		return -1;
-	}
+  reply = redisCommand(context, "PUBLISH %s%d %s", redis_channel, communication_port, j);
+  free(j);
+  if(reply)
+  {
+    freeReplyObject(reply);
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
 /**
@@ -168,6 +168,6 @@ int sendMessage (char *id, int communication_port, char *data)
  */
 int closeCommunication ()
 {
-	redisFree(context);
-	redisAsyncDisconnect(c);
+  redisFree(context);
+  redisAsyncDisconnect(c);
 }
