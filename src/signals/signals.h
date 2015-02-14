4cba9c82e73724dce68af73585cb078ca6dba72b
Razvan Madalin MATEI <matei.rm94@gmail.com>
2015-02-14 18:30:52 +0200

    doc: OK, maybe update description

--- src/signals/signals.h
+++ src/signals/signals.h
@@ -1,4 +1,10 @@
-
+/**************************************************************************************************
+ * Signals
+ *
+ * CONTENT
+ * 1. Signals
+ *************************************************************************************************/
+ 
 #ifndef SIGNALS__H
 #define SIGNALS__H
 
@@ -6,17 +12,23 @@
 extern "C" {
 #endif
 
-// timestamps and sends values
-int initSignal(int port, const char *pId, const char *sId, const char *userid);
-int sendSignal  (const char *name, double value);
-int sendSignalAndFlag (const char *flag, const char *name, double value);
-int sendSignalsAndFlag (const char *flag, const char *name, double value, ...);
-int sendSignals  (const char *name, double value, ...);
-int putFlag (const char *sigal, const char *flag);
-int sendSignalsList (const char **names, double *values, int elements);
-int sendSignalsListAndFlag  (const char *flag, const char **names, double *values, int elements);
-int sendCoordinates(const char *name, double latitude, double longitude);
-int sendCoordinatesAndFlag(const char *name, double latitude, double longitude, const char *flag);
+
+
+/**************************************************************************************************
+ * 1. Signals
+ *************************************************************************************************/
+
+int initSignal             (int port, const char *pId, const char *sId, const char *userid);
+int sendSignal             (const char *name, double value);
+int sendSignalAndFlag      (const char *flag, const char *name, double value);
+int sendSignalsAndFlag     (const char *flag, const char *name, double value, ...);
+int sendSignals            (const char *name, double value, ...);
+int putFlag                (const char *sigal, const char *flag);
+int sendSignalsList        (const char **names, double *values, int elements);
+int sendSignalsListAndFlag (const char *flag, const char **names, double *values, int elements);
+int sendCoordinates        (const char *name, double latitude, double longitude);
+int sendCoordinatesAndFlag (const char *name, double latitude, double longitude, const char *flag);
+
 
 
 #ifdef __cplusplus
