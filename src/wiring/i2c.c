cbe0fd8b6a20699109427b88e499083ad44d60c8
Razvan Madalin MATEI <matei.rm94@gmail.com>
2015-02-16 17:15:41 +0200

    doc: convert indentation to spaces

--- src/wiring/i2c.c
+++ src/wiring/i2c.c
@@ -38,12 +38,12 @@ pthread_mutex_t locki2c;
  */
 void i2c_init ()
 {
-	int i;
+  int i;
 
-	for (i = 0; i < MAX_I2C_PINS; i++) 
-	{
-		i2c_buses[i] = -1;
-	}
+  for (i = 0; i < MAX_I2C_PINS; i++) 
+  {
+    i2c_buses[i] = -1;
+  }
 }
 
 /**
@@ -51,21 +51,21 @@ void i2c_init ()
  */
 int getI2CId ()
 {
-	int i;
-	int id = -1;
+  int i;
+  int id = -1;
 
-	pthread_mutex_lock(&locki2c);
-	for (i = 0; i < MAX_I2C_BUSES && id == -1; i++)
-	{
-		if (i2c_buses[i] == -1)
-		{
-			id = i;
-			i2c_buses[id] = 0;
-		}
-	}
-	pthread_mutex_unlock(&locki2c);
+  pthread_mutex_lock(&locki2c);
+  for (i = 0; i < MAX_I2C_BUSES && id == -1; i++)
+  {
+    if (i2c_buses[i] == -1)
+    {
+      id = i;
+      i2c_buses[id] = 0;
+    }
+  }
+  pthread_mutex_unlock(&locki2c);
 
-	return id;
+  return id;
 }
 
 /**
@@ -73,9 +73,9 @@ int getI2CId ()
  */
 void releaseI2CId (int id)
 {
-	pthread_mutex_lock(&locki2c);
-	i2c_buses[id] = -1;
-	pthread_mutex_unlock(&locki2c);
+  pthread_mutex_lock(&locki2c);
+  i2c_buses[id] = -1;
+  pthread_mutex_unlock(&locki2c);
 }
 
 /**
@@ -83,7 +83,7 @@ void releaseI2CId (int id)
  */
 int i2c_getadapter(uint32_t i2c_bus_address)
 {
-	return -1;
+  return -1;
 }
 
 /**
@@ -91,7 +91,7 @@ int i2c_getadapter(uint32_t i2c_bus_address)
  */
 int i2c_add_to_buf(uint8_t addr, uint8_t rw, uint8_t *value, int length)
 {
-	return -1;
+  return -1;
 }
 
 
@@ -105,37 +105,37 @@ int i2c_add_to_buf(uint8_t addr, uint8_t rw, uint8_t *value, int length)
  */
 int i2c_openadapter(uint8_t i2c_bus)
 {
-	int id;
-	char filepath[32];
+  int id;
+  char filepath[32];
 
-	id = getI2CId();
+  id = getI2CId();
 
-	if (i2c_bus == 255)
-	{
-		#ifdef RASPBERRYPI
-		if (piBoardRev() == 1)
-		{
-			i2c_bus = 0;
-		}
-		else
-		{
-			i2c_bus = 1;
-		}
-		#endif
-		
-		#ifdef BEAGLEBONE
-		i2c_bus = 1;
-		#endif
-	}
+  if (i2c_bus == 255)
+  {
+    #ifdef RASPBERRYPI
+    if (piBoardRev() == 1)
+    {
+      i2c_bus = 0;
+    }
+    else
+    {
+      i2c_bus = 1;
+    }
+    #endif
+    
+    #ifdef BEAGLEBONE
+    i2c_bus = 1;
+    #endif
+  }
 
-	snprintf(filepath, 32, "/dev/i2c-%u", i2c_bus);
+  snprintf(filepath, 32, "/dev/i2c-%u", i2c_bus);
   if ((i2c_buses[id] = open(filepath, O_RDWR)) < 0) 
   {
     fprintf(stderr, "Failed to open requested i2c port %s\n", filepath);
-		perror("bus");
+    perror("bus");
   }
-	
-	return id;
+  
+  return id;
 }
 
 /**
@@ -143,11 +143,11 @@ int i2c_openadapter(uint8_t i2c_bus)
  */
 int i2c_closeadapter(int i2c_id)
 {
-	int rc = close(i2c_buses[i2c_id]);
-	i2c_addresses[i2c_id] = -1;
-	releaseI2CId (i2c_id);
+  int rc = close(i2c_buses[i2c_id]);
+  i2c_addresses[i2c_id] = -1;
+  releaseI2CId (i2c_id);
 
-	return rc;
+  return rc;
 }
 
 /**
@@ -155,8 +155,8 @@ int i2c_closeadapter(int i2c_id)
  */
 int i2c_setslave(int i2c_id, uint8_t addr)
 {
-	ioctl(i2c_buses[i2c_id], I2C_SLAVE_FORCE, addr);
-	return 0;
+  ioctl(i2c_buses[i2c_id], I2C_SLAVE_FORCE, addr);
+  return 0;
 }
 
 /**
@@ -164,30 +164,30 @@ int i2c_setslave(int i2c_id, uint8_t addr)
  */
 int i2c_writebyte(int i2c_id, uint8_t byte)
 {
-	if(i2c_smbus_write_byte(i2c_buses[i2c_id], byte) < 0)
-	{
+  if(i2c_smbus_write_byte(i2c_buses[i2c_id], byte) < 0)
+  {
     fprintf(stderr, "Failed to write to i2c\n");
-		perror("i2c write");
+    perror("i2c write");
   }
 
-	return 0;
+  return 0;
 }
 
 /**
  * Write bytes on I2C bus
  */
 int i2c_writebytes(int i2c_id, uint8_t *bytes, uint8_t length){
-	int i;
-	int rc = 0;
-	int fd = i2c_buses[i2c_id];
+  int i;
+  int rc = 0;
+  int fd = i2c_buses[i2c_id];
 
-	if(i2c_smbus_write_i2c_block_data(i2c_buses[i2c_id], bytes[0], length-1, (uint8_t*) bytes+1) < 0)
-	{
-		fprintf(stderr, "Failed to write to i2c\n");
-		perror ("i2c write bytes");
+  if(i2c_smbus_write_i2c_block_data(i2c_buses[i2c_id], bytes[0], length-1, (uint8_t*) bytes+1) < 0)
+  {
+    fprintf(stderr, "Failed to write to i2c\n");
+    perror ("i2c write bytes");
   }
   
-	return rc;
+  return rc;
 }
 
 /**
@@ -195,16 +195,16 @@ int i2c_writebytes(int i2c_id, uint8_t *bytes, uint8_t length){
  */
 int i2c_readbyte(int i2c_id)
 {
-	int data = i2c_smbus_read_byte(i2c_buses[i2c_id]);
-	
-	if (data < 0) 
-	{
-		return -1;
-	}
-	else
-	{
-		return data;
-	}	
+  int data = i2c_smbus_read_byte(i2c_buses[i2c_id]);
+  
+  if (data < 0) 
+  {
+    return -1;
+  }
+  else
+  {
+    return data;
+  } 
 }
 
 /**
@@ -212,11 +212,11 @@ int i2c_readbyte(int i2c_id)
  */
 int i2c_readbytes(int i2c_id, uint8_t *buf, int length)
 {
-	if(read(i2c_buses[i2c_id], buf, length) == length) {
+  if(read(i2c_buses[i2c_id], buf, length) == length) {
     return length;
   }
 
-	return 0;
+  return 0;
 }
 
 /**
@@ -224,7 +224,7 @@ int i2c_readbytes(int i2c_id, uint8_t *buf, int length)
  */
 int i2c_readwrite(int i2c_id)
 {
-	return -1;
+  return -1;
 }
 
 
