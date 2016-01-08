acce3b2451745dedbcb9c1001ed646fc011f6178
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2016-01-08 13:54:23 +0200

    add udooneo stubs

---
+++ src/wiring/udooneo.c
@@ -0,0 +1,202 @@
+#ifdef UDOONEO
+
+
+
+/*** INCLUDES ************************************************************************************/
+
+#include "wiring.h"
+
+/*************************************************************************************************/
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+
+
+/*** API IMPLEMENTATION **************************************************************************/
+
+int wiringSetup() {
+  return 0;
+}
+
+void pinReset(int pin) {
+
+}
+
+/* I/O */
+void pinMode        (int pin, int mode) {
+
+}
+
+void digitalWrite   (int pin, int value) {
+
+}
+
+int  digitalRead    (int pin) {
+  return 0;
+}
+
+void analogWrite    (int pin, int value) {
+
+}
+
+int  analogRead     (int pin) {
+  return 0;
+}
+
+int  analogReadRaw  (int pin) {
+  return 0;
+}
+
+void analogWriteRaw (int pin, int value) {
+
+}
+
+/* Advanced I/O */
+void          shiftOut (uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val) {
+
+}
+
+uint8_t       shiftIn  (uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder) {
+  return 0;
+}
+
+unsigned long pulseIn  (uint8_t pin, uint8_t state) {
+  return 0;
+}
+
+/* Time */
+void delay             (unsigned int milliseconds) {
+
+}
+
+void delayMicroseconds (unsigned int microseconds) {
+
+}
+
+unsigned int millis    (void) {
+  return 0;
+}
+
+unsigned int micros    (void) {
+  return 0;
+}
+
+int             spi_getadapter    (uint32_t spi_bus_address) {
+  return 0;
+}
+
+int             spi_openadapter   (uint8_t spi_bus) {
+  return 0;
+}
+
+int             spi_setmode       (int spiId, unsigned short mode) {
+  return 0;
+}
+
+int             spi_set_frequency (int spiId, int freq) {
+  return 0;
+}
+
+uint8_t         spi_writebyte     (int spiId, uint8_t byte) {
+  return 0;
+}
+
+unsigned char * spi_writebytes    (int spiId, uint8_t *bytes, uint8_t length) {
+  return NULL;
+}
+
+int             spi_lsb_mode      (int spiId, unsigned char lsb) {
+  return 0;
+}
+
+int             spi_bit_per_word  (int spiId, unsigned int bits) {
+  return 0;
+}
+
+int             spi_closeadapter  (int spiId) {
+  return 0;
+}
+
+int i2c_openadapter  (uint8_t i2c_bus) {
+  return 0;
+}
+
+int i2c_setslave     (int i2cId, uint8_t addr) {
+  return 0;
+}
+
+int i2c_writebyte    (int i2cId, uint8_t byte) {
+  return 0;
+}
+
+int i2c_writebytes   (int i2cId, uint8_t *bytes, uint8_t length) {
+  return 0;
+}
+
+int i2c_readbyte     (int i2cId) {
+  return 0;
+}
+
+int i2c_closeadapter (int i2cId) {
+  return 0;
+}
+
+int i2c_readbytes    (int i2cId, uint8_t *buf, int length) {
+  return 0;
+}
+
+int i2c_readwrite    (int i2cId) {
+  return 0;
+}
+
+
+
+int     serial_openadapter     (char *serial_bus) {
+  return 0;
+}
+
+int     serial_set_speed       (int serial_id, int baud) {
+  return 0;
+}
+
+int     serial_bytes_available (int serial_id) {
+  return 0;
+}
+
+int     serial_closeadapter    (int serial_id) {
+  return 0;
+}
+
+int     serial_writebyte       (int serial_id, uint8_t byte) {
+  return 0;
+}
+
+int     serial_writebytes      (int serial_id, uint8_t *bytes, uint8_t length) {
+  return 0;
+}
+
+uint8_t serial_readbyte        (int serial_id) {
+  return 0;
+}
+
+int     serial_readbytes       (int serial_id, uint8_t *buf, int length) {
+  return 0;
+}
+
+int     serial_flush           (int serial_id) {
+  return 0;
+}
+
+/*************************************************************************************************/
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* UDOONEO */
--- src/wiring/wiring.h
+++ src/wiring/wiring.h
@@ -1,207 +1,188 @@
 #ifndef _WIRING_H
 #define _WIRING_H
 
-#define	LSBFIRST	0
-#define	MSBFIRST	1
-#define MAX_I2C_BUSES 10
-#define MAX_SPI_BUSES 10
-#define MAX_SERIAL_BUSES	10
+
+
+/*** DEFINES *************************************************************************************/
+
+#define LSBFIRST  0
+#define MSBFIRST  1
+
+#define MAX_I2C_BUSES    10
+#define MAX_SPI_BUSES    10
+#define MAX_SERIAL_BUSES 10
 
 #define RUN_RASPBERRYPI 0
-#define RUN_GALILEO 1
-#define RUN_EDISON 2
-#define RUN_BEAGLEBONE 3
-#define RUN_SERVER 5
+#define RUN_GALILEO     1
+#define RUN_EDISON      2
+#define RUN_BEAGLEBONE  3
+#define RUN_SERVER      5
 
 #ifdef RASPBERRYPI
-#define BOARD RUN_RASPBERRYPI
+  #define BOARD RUN_RASPBERRYPI
 #endif /* RASPBERRYPI */
 
 #ifdef ARDUINOGALILEO
-#define BOARD RUN_GALILEO
+  #define BOARD RUN_GALILEO
 #endif /* ARDUINOGALILEO */
 
 #ifdef EDISON
-#define BOARD RUN_EDISON
+  #define BOARD RUN_EDISON
 #endif /* EDISON */
 
 #ifdef BEAGLEBONE
-#define BOARD RUN_BEAGLEBONE
+  #define BOARD RUN_BEAGLEBONE
 #endif /* BEAGLEBONE */
 
 #ifdef SERVER
-#define BOARD RUN_SERVER
+  #define BOARD RUN_SERVER
 #endif /* SERVER */
 
 #ifdef REDPITAYA
-#define BOARD RUN_REDPITAYA
+  #define BOARD RUN_REDPITAYA
 #endif /* REDPITAYA */
 
-typedef unsigned char uint8_t;
+#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
+
+#define max(x, y) (((x) > (y)) ? (x) : (y))
+#define min(x, y) (((x) < (y)) ? (x) : (y))
+
+#define LSBFIRST  0
+#define MSBFIRST  1
+
+#define HIGH   1
+#define LOW    0
+#define INPUT  0
+#define OUTPUT 1
+
+/*************************************************************************************************/
+
+
+
+/*** INCLUDES ************************************************************************************/
 
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 
-#define pgm_read_byte(addr) (*(const unsigned char *)(addr))
-#define max(x, y) (((x) > (y)) ? (x) : (y))
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
-
-// Raspberry Pi
-// use wiring library
-#ifdef RASPBERRYPI
-#include <wiringPi.h>
-#include "raspberrypi.h"
-#define MAX_I2C_PINS	10
-#endif /* RASPBERRYPI */
-
-// Arduino Galileo
-// use the mraa library
-#if defined(ARDUINOGALILEO) || defined(EDISON)
-
-#include <mraa/gpio.h>
-#include <mraa/aio.h>
-#include <mraa/pwm.h>
-#include <mraa/i2c.h>
-#include <mraa/spi.h>
-
-#define MAX_GPIO_PINS 100
-#define MAX_AIO_PINS 40
-#define MAX_PWM_PINS 50
-
-#define A0	14
-#define A1	15
-#define A2	16
-#define A3	17
-#define A4	18
-#define A5	19
-#define A6	20
-
-#endif /* defined(ARDUINOGALILEO) || defined(EDISON) */
-
-#ifdef REDPITAYA
-#define A0	0
-#define A1	1
-#define A2	2
-#define A3	3
-#endif /* REDPITAYA */
-
-#define LSBFIRST  0
-#define MSBFIRST  1
-
-#define HIGH 1
-#define LOW 0
-#define INPUT 0
-#define OUTPUT 1
-
-
-
 #include "binary.h"
 #include "BitsAndBytes.h"
 
-typedef uint8_t byte;
-typedef unsigned short uint16_t;
+#ifdef RASPBERRYPI
+  #include <wiringPi.h>
+  #include "raspberrypi.h"
+  #define MAX_I2C_PINS  10
+#endif /* RASPBERRYPI */
+
+#if defined(ARDUINOGALILEO) || defined(EDISON)
+  #include <mraa/gpio.h>
+  #include <mraa/aio.h>
+  #include <mraa/pwm.h>
+  #include <mraa/i2c.h>
+  #include <mraa/spi.h>
+
+  #define MAX_GPIO_PINS 100
+  #define MAX_AIO_PINS 40
+  #define MAX_PWM_PINS 50
+
+  #define A0  14
+  #define A1  15
+  #define A2  16
+  #define A3  17
+  #define A4  18
+  #define A5  19
+  #define A6  20
+#endif /* defined(ARDUINOGALILEO) || defined(EDISON) */
+
+#ifdef REDPITAYA
+  #define A0  0
+  #define A1  1
+  #define A2  2
+  #define A3  3
+#endif /* REDPITAYA */
+
+/*************************************************************************************************/
+
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
+
+
+/*** API *****************************************************************************************/
+
 #ifdef REDPITAYA
-void analogWriteVoltage (int pin, float value);
-float analogReadVoltage (int pin);
+  void  analogWriteVoltage (int pin, float value);
+  float analogReadVoltage  (int pin);
 #endif /* REDPITAYA */
 
-int wiringSetup ();
+int wiringSetup();
 
-void pinReset (int pin);
+void pinReset(int pin);
 
-// WIRING
-void pinMode (int pin, int mode);
-void digitalWrite (int pin, int value);
-int digitalRead (int pin);
-void analogWrite (int pin, int value);
-int analogRead (int pin);
-int analogReadRaw (int pin);
+/* I/O */
+void pinMode        (int pin, int mode);
+void digitalWrite   (int pin, int value);
+int  digitalRead    (int pin);
+void analogWrite    (int pin, int value);
+int  analogRead     (int pin);
+int  analogReadRaw  (int pin);
 void analogWriteRaw (int pin, int value);
 
-//#ifndef RASPBERRYPI
-void delay (unsigned int milliseconds);
+/* Advanced I/O */
+void          shiftOut (uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+uint8_t       shiftIn  (uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+unsigned long pulseIn  (uint8_t pin, uint8_t state);
+
+/* Time */
+void delay             (unsigned int milliseconds);
 void delayMicroseconds (unsigned int microseconds);
-unsigned int millis (void);
-unsigned int micros (void);
-//#endif
+unsigned int millis    (void);
+unsigned int micros    (void);
 
-// Shift
+long randomMinMax(long, long);
 
-unsigned long pulseIn(uint8_t pin, uint8_t state);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
-uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
-// void tone(uint8_t _pin, unsigned int frequency, unsigned long duration);
-// void noTone(uint8_t _pin);
+long map(long, long, long, long, long);
 
-// Math
-long randomMinMax( long, long ) ;
-
-long map( long, long, long, long, long ) ;
-
-uint16_t makeWord( uint8_t h, uint8_t l ) ;
+uint16_t makeWord(uint8_t h, uint8_t l);
 
 #define word(...) makeWord(__VA_ARGS__)
 
-// Server
-#ifdef SERVER
-int wiringSetup ();
-#endif /* SERVER */
+int             spi_getadapter    (uint32_t spi_bus_address);
+int             spi_openadapter   (uint8_t spi_bus);
+int             spi_setmode       (int spiId, unsigned short mode);
+int             spi_set_frequency (int spiId, int freq);
+uint8_t         spi_writebyte     (int spiId, uint8_t byte);
+unsigned char * spi_writebytes    (int spiId, uint8_t *bytes, uint8_t length);
+int             spi_lsb_mode      (int spiId, unsigned char lsb);
+int             spi_bit_per_word  (int spiId, unsigned int bits);
+int             spi_closeadapter  (int spiId);
 
-// SPI
-
-// TODO
-int spi_getadapter(uint32_t spi_bus_address);
-int spi_openadapter(uint8_t spi_bus);
-int spi_setmode(int spiId, unsigned short mode);
-int spi_set_frequency(int spiId, int freq);
-uint8_t spi_writebyte(int spiId, uint8_t byte);
-unsigned char * spi_writebytes(int spiId, uint8_t *bytes, uint8_t length);
-int spi_lsb_mode(int spiId, unsigned char lsb);
-int spi_bit_per_word(int spiId, unsigned int bits);
-int spi_closeadapter (int spiId);
-
-// I2C
-
-// int i2c_getadapter(uint32_t i2c_bus_address);
-int i2c_openadapter(uint8_t i2c_bus);
-int i2c_setslave(int i2cId, uint8_t addr);
-int i2c_writebyte(int i2cId, uint8_t byte);
-int i2c_writebytes(int i2cId, uint8_t *bytes, uint8_t length);
-int i2c_readbyte(int i2cId);
+int i2c_openadapter  (uint8_t i2c_bus);
+int i2c_setslave     (int i2cId, uint8_t addr);
+int i2c_writebyte    (int i2cId, uint8_t byte);
+int i2c_writebytes   (int i2cId, uint8_t *bytes, uint8_t length);
+int i2c_readbyte     (int i2cId);
 int i2c_closeadapter (int i2cId);
-int i2c_readbytes(int i2cId, uint8_t *buf, int length);
-int i2c_readwrite(int i2cId);
-// int i2c_add_to_buf(uint8_t addr, uint8_t rw, uint8_t *value, int length);
+int i2c_readbytes    (int i2cId, uint8_t *buf, int length);
+int i2c_readwrite    (int i2cId);
 
-// Serial
 
-int serial_openadapter(char *serial_bus);
-int serial_set_speed(int serial_id, int baud);
-int serial_bytes_available(int serial_id);
-int serial_closeadapter(int serial_id);
-int serial_writebyte(int serial_id, uint8_t byte);
-int serial_writebytes(int serial_id, uint8_t *bytes, uint8_t length);
-uint8_t serial_readbyte(int serial_id);
-int serial_readbytes(int serial_id, uint8_t *buf, int length);
-int serial_flush(int serial_id);
-/* TODO if needed
-	availalble
-	find
-	findUntil
-	parseFloat
-	parseInt
-	peek
-	readBytesUntil
-	setTimeout-nu se prea poate din wiringpi
-	serialEvent
-*/
+int     serial_openadapter     (char *serial_bus);
+int     serial_set_speed       (int serial_id, int baud);
+int     serial_bytes_available (int serial_id);
+int     serial_closeadapter    (int serial_id);
+int     serial_writebyte       (int serial_id, uint8_t byte);
+int     serial_writebytes      (int serial_id, uint8_t *bytes, uint8_t length);
+uint8_t serial_readbyte        (int serial_id);
+int     serial_readbytes       (int serial_id, uint8_t *buf, int length);
+int     serial_flush           (int serial_id);
+
+/*************************************************************************************************/
+
+
 
 #ifdef __cplusplus
 }
